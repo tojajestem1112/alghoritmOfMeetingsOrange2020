b8aee6766c3bac03b5709468c8550f654de0cf60
tojajestem1112 <jalowskidaw@gmail.com>
2020-06-17 18:17:43 +0200

    Added Time class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,11 @@ public class Main {
 
     public static void main(String args[])
     {
-        System.out.println("Hello world");
+        Time time1 = new Time(9,0);
+        Time time2 = new Time(12,50);
+
+        time1.addTime(10,12);
+        time2.addTime(12,13);
+        System.out.println(time1 + "\n" + time2);
     }
 }
---
+++ src/main/java/Time.java
@@ -0,0 +1,64 @@
+public class Time
+{
+    private int hours;
+    private int minutes;
+
+    public Time()
+    {
+        int hours = 0;
+        int minutes = 0;
+    }
+
+    public Time(int hours, int minutes)
+    {
+        if(hours<0 || minutes <0)
+            throw new ArithmeticException("Number of minutes and hours must be positive!");
+
+        this.hours = hours;
+        this.minutes = minutes;
+        checkTime();
+    }
+
+    private void checkTime()
+    {
+        while(minutes >= 60)
+        {
+            hours++;
+            minutes-=60;
+        }
+        while(hours >=24)
+        {
+            hours-= 24;
+        }
+    }
+
+    public void addTime(int hours, int minutes)
+    {
+        this.minutes += minutes;
+        this.hours += hours;
+        checkTime();
+    }
+
+    private String getNumberWithTwoDigits(int number)
+    {
+        String numberWithTwoDigits = String.valueOf(number);
+        if(numberWithTwoDigits.length()  < 2)
+        {
+            numberWithTwoDigits = "0" + numberWithTwoDigits;
+        }
+        return numberWithTwoDigits;
+    }
+
+    public int getHours() {
+        return hours;
+    }
+
+    public int getMinutes() {
+        return minutes;
+    }
+
+    public String toString()
+    {
+        return getNumberWithTwoDigits(hours) + " : "+ getNumberWithTwoDigits(minutes);
+    }
+}
